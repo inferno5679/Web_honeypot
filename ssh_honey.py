5af9b8d5d561f8965a734e788f34cd26d7d40f73
Arpan Doogar <arpandoogar@gmail.com>
2024-09-24 01:30:44 +0530

    Added logging to capture ipaddresses and packets and formatting for the logs

--- ssh_honey.py
+++ ssh_honey.py
@@ -1,1 +1,10 @@
 import logging
+from logging.handlers import RotatingFileHandler
+
+logging_format = logging.Formatter('%(message)s')
+
+funnel_logger = logging.getLogger('FunnelLogger')
+funnel_logger.setLevel(logging.INFO)
+funnel_handler = RotatingFileHandler("audits.log",maxBytes=1000,backupCount=5)
+funnel_handler.setFormatter(logging_format)
+funnel_logger.addHandler(funnel_handler)
