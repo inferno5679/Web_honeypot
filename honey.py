f66dbbe6a356cf632ae158fd3e5c46c8bd109db8
Arpan Doogar <arpandoogar@gmail.com>
2024-09-26 00:07:26 +0530

    Added more python files which will be required for the course of this project and divided files into segments by commenting what goes where and who does it

---
+++ honey.py
@@ -0,0 +1,1 @@
+# Honeypy file - Suresh
--- ssh_honey.py
+++ ssh_honey.py
@@ -1,5 +1,8 @@
+# libraries
 import logging
 from logging.handlers import RotatingFileHandler
+
+# Logging
 
 logging_format = logging.Formatter('%(message)s')
 
@@ -8,3 +11,9 @@ funnel_logger.setLevel(logging.INFO)
 funnel_handler = RotatingFileHandler("audits.log",maxBytes=1000,backupCount=5)
 funnel_handler.setFormatter(logging_format)
 funnel_logger.addHandler(funnel_handler)
+
+# Emulated Shell - Arpan
+
+# SSH-server + Sockets - wamiq
+
+# Provision based honeypot - wamiq
