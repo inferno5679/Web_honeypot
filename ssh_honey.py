7226a923779681ef9194ae241e1f3569fe126333
Arpan Doogar <arpandoogar@gmail.com>
2024-09-30 21:22:46 +0530

    Added credential logger to the code

--- ssh_honey.py
+++ ssh_honey.py
@@ -12,6 +12,12 @@ funnel_handler = RotatingFileHandler("audits.log",maxBytes=1000,backupCount=5)
 funnel_handler.setFormatter(logging_format)
 funnel_logger.addHandler(funnel_handler)
 
+creds_logger = logging.getLogger('CredsLogger')
+creds_logger.setLevel(logging.INFO)
+creds_handler = RotatingFileHandler("cmd_audits.log",maxBytes=1000,backupCount=5)
+creds_handler.setFormatter(logging_format)
+creds_logger.addHandler(creds_handler)
+
 # Emulated Shell - Arpan
 
 # SSH-server + Sockets - Sureshkumar + wamiq
